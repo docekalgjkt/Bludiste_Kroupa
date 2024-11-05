6df9b2c4c1a32935bca25477612a1c9e7c768fac
mkroupa21 <matej.kroupa@gjkt.cz>
2024-11-05 14:20:36 +0100

    změna

---
+++ DOA imple.py
@@ -0,0 +1,11 @@
+Bludiště bylo uloženo do souboru 'bludiste.txt'.
+Bludiště bylo načteno ze souboru 'bludiste.txt':
+Šířka: 4
+Výška: 4
+Startovní pozice: (0, 0)
+Východ: (3, 3)
+Mapa:
+[0, 1, 0, 0]
+[0, 1, 0, 1]
+[0, 0, 0, 1]
+[1, 1, 0, 'E']
